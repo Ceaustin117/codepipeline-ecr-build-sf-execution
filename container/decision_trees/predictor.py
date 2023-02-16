9c0b69eeae472f9bc616f3947eaee9d726793142
Chris Austin <chris.austin@wavicledata.com>
2023-02-15 18:34:51 -0600

    printing

--- container/decision_trees/predictor.py
+++ container/decision_trees/predictor.py
@@ -97,6 +97,7 @@ def transformation():
     data = [[0, 2633, '2019-01-13', 34457, 84, 3], [ 1, 2633, '2019-01-20', 34457, 144, 3]]
     fake_pred_data = pd.DataFrame(data, columns=[150, 'store', 'date', 'item', 'bottles_sold', 'flag'])
     print("fake_pred_data!\n\n")
+    print("\n")
     print(fake_pred_data)
     fake_pred_data = fake_pred_data.loc[fake_pred_data['date'] < pd.to_datetime('12-01-2021', format='%m-%d-%Y')]
     # [0, 150, 'store', 'date', 'item', 'bottles_sold', 'flag'],
