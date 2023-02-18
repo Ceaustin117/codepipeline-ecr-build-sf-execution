ed6aaa0a97d4f60e8e0c913bfe56d6011620e56d
Chris Austin <chris.austin@wavicledata.com>
2023-02-17 18:28:57 -0600

    train test

--- container/decision_trees/optimize_ets_model.py
+++ container/decision_trees/optimize_ets_model.py
@@ -93,7 +93,7 @@ def optimize_ETS(data, target, date, flag, errors, trends, dampened_trends, seas
                             except:
                                 continue
     
-    train, test = train_test(data, date)
+    train = train_test(data, date)
     model = ETSModel(train[target], error=best_params[0], trend=best_params[1], damped_trend=best_params[2], seasonal=best_params[3], seasonal_periods=best_params[4], initialization_method=best_params[5])
     model_fit = model.fit(disp=0)
     predictions = np.array([x for x in model_fit.predict(start = 0, end = (len(train) + (horizons[horiz]-1)))])
