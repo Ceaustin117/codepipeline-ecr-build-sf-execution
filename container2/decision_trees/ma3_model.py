bacdb0f83e01c49723578bee375872d9e12778ba
Chris Austin <chris.austin@wavicledata.com>
2023-02-21 17:55:24 -0600

    update all

---
+++ container2/decision_trees/ma3_model.py
@@ -0,0 +1,60 @@
+#Import packages
+import pandas as pd
+import numpy as np
+import os
+import json
+import pickle
+from statsmodels.tsa.arima.model import ARIMA
+
+#Import local function
+from train_test_function import train_test
+
+# These are the paths to where SageMaker mounts interesting things in your container.
+
+prefix = '/opt/ml/'
+
+input_path = prefix + 'input/data'
+output_path = os.path.join(prefix, 'output')
+model_path = os.path.join(prefix, 'model')
+param_path = os.path.join(prefix, 'input/config/hyperparameters.json')
+
+# This algorithm has a single channel of input data called 'training'. Since we run in
+# File mode, the input files are copied to the directory specified here.
+channel_name='training'
+training_path = os.path.join(input_path, channel_name)
+
+#Import horizons dictionary
+#Based on determined forecastability thresholds, which were jointly determined by Michael, Patrick and Mitch, the horizons dictionary maps the determined Fcast_flag to the # of periods the given series is able to be forecast.
+    #NOTE: These thresholds were determined based on the data being aggregated to a weekly level. Should this aggregation level change, please address this in the set_forecast_flag() function.
+        #The breakdown of forecast flag values is as follows:
+            #0: The given series has <20 non-zero values. Non-forecastable
+            #1: The given series has between 20 and 48 non-zero values. Forecastable to 1 period (1wk) out
+            #2: The given series has between 49 and 120 non-zero values. Forecastable to 4 periods (1mo) out
+            #3: The given series has >120 non-zero values. Forecastable to 12 periods (1qtr) out
+horizons = {}
+with open("horizons.txt") as f:
+    horizons = json.load(f)
+
+def MA3(df, target, date, flag):
+    """Predicts target variable using average of prior 3 items in series. Will be used for every time series regardless of level of 'forecastability'
+
+    Args:
+        df (pandas DataFrame): dataframe used for pipeline procedures
+        target (str): target column name found in df
+        date (str): date column name found in df
+
+    Returns:
+        array: 3-point moving average forecast results equal to length of time series. .
+    """
+    horiz = df['flag'].mean().astype('int').astype('str')
+    train = train_test(df, date)
+    if len(train[target]) >= 3:
+        model = ARIMA(train[target], order = (0,0,3)).fit()
+        pred = np.array([x for x in model.predict(start = 0, end = (len(train) + (horizons[horiz]-1)))])
+
+        # pred.fillna(np.mean(train[target]), inplace = True)
+    else:
+        pred = np.mean(df[target])
+
+    print("time to return model")
+    return model
---
+++ container2/decision_trees/predictor.py
@@ -0,0 +1,104 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to
+# implement the scoring for your own algorithm.
+
+from __future__ import print_function
+
+import os
+import json
+import pickle
+from io import StringIO
+import sys
+import signal
+import traceback
+import numpy as np
+
+import flask
+
+import pandas as pd
+
+prefix = '/opt/ml/'
+model_path = os.path.join(prefix, 'model')
+
+# A singleton for holding the model. This simply loads the model and holds it.
+# It has a predict function that does a prediction based on the model and the input data.
+
+class ScoringService(object):
+    model = None                # Where we keep the model when it's loaded
+
+    @classmethod
+    def get_model(cls):
+        """Get the model object for this instance, loading it if it's not already loaded."""
+        if cls.model == None:
+            with open(os.path.join(model_path, 'decision-tree-model.pkl'), 'rb') as inp:
+                cls.model = pickle.load(inp,encoding="latin1")
+        return cls.model
+
+    @classmethod
+    def predict(cls, input):
+        print("inside predict")
+        """For the input, do the predictions and return them.
+
+        Args:
+            input (a pandas dataframe): The data on which to do the predictions. There will be
+                one prediction per row in the dataframe"""
+        clf = cls.get_model()
+        return clf.predict(start = 0, end = 3)
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = ScoringService.get_model() is not None  # You can insert a health check here
+
+    status = 200 if health else 404
+    return flask.Response(response='\n', status=status, mimetype='application/json')
+
+@app.route('/invocations', methods=['POST'])
+def transformation():
+    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
+    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
+    just means one prediction per line, since there's a single column..
+    """
+    data = None
+
+    # Convert from CSV to pandas.
+    if flask.request.content_type == 'text/csv':
+        data = flask.request.data.decode('utf-8')
+        s = StringIO(data)
+        data = pd.read_csv(s, header=None)
+    else:
+        return flask.Response(response='This predictor only supports CSV data', status=415, mimetype='text/plain')
+    
+    data = data.rename(columns=str).rename(columns={'nan':'new_lbl'})
+    data.columns=data.iloc[0]
+    data=data.drop(data.index[[0]])
+    data['date'] = pd.to_datetime(data['date'], errors='coerce')
+    data['date'] = pd.to_datetime(data['date'], errors='coerce', format = '%Y-%m-%d')
+    data['flag'] = 3
+    test_data = data.loc[data['date'] >= pd.to_datetime('12-01-2021', format='%m-%d-%Y')]
+    if len(test_data) == 0:
+        train_size = np.round((0.7*len(data)),0).astype('int')
+        test_data = test_data[train_size:]
+    
+    # initialize list of lists
+    try:
+        data = [[1,0, 2633, '2019-01-13', 34457, 84, 3], [ 2,1, 2633, '2019-01-20', 34457, 144, 3]]
+        fake_pred_data = pd.DataFrame(data, columns=[0,150, 'store', 'date', 'item', 'bottles_sold', 'flag'])
+        fake_pred_data['date'] = pd.to_datetime(fake_pred_data['date'], errors='coerce')
+        fake_pred_data.index = list(fake_pred_data.index)
+        fake_pred_data = fake_pred_data.loc[fake_pred_data['date'] < pd.to_datetime('12-01-2021', format='%m-%d-%Y')]
+        fake_pred_data.index = np.arange(1, len(fake_pred_data) + 1)
+        print("time to predict")
+        predictions = ScoringService.predict(fake_pred_data['bottles_sold'])
+        # Convert from numpy back to CSV
+        out = StringIO()
+        pd.DataFrame({'results':predictions}).to_csv(out, header=0, index=False)
+        result = out.getvalue()
+    except Exception as e:
+        print('Exception during training: ' + str(e))
+
+
+    return flask.Response(response=result, status=200, mimetype='text/csv')
