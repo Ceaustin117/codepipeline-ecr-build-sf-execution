f5f0d7fae80e4ff640ddcfb18716772038c787d2
Chris Austin <chris.austin@wavicledata.com>
2023-02-28 20:11:03 -0600

    jj

--- container_3ma/decision_trees/predictor.py
+++ container_3ma/decision_trees/predictor.py
@@ -42,7 +42,7 @@ class ScoringService(object):
             input (a pandas dataframe): The data on which to do the predictions. There will be
                 one prediction per row in the dataframe"""
         clf = cls.get_model()
-        return clf.predict(start = data[0], end = data[1])
+        return clf.predict(start = int(data[0]), end = int(data[1]))
 
 # The flask app for serving predictions
 app = flask.Flask(__name__)
